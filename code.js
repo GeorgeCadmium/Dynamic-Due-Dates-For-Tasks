be15f43c5d0e0c84934cd81e4f41140686e03b2f
GeorgeCadmium <60203369+GeorgeCadmium@users.noreply.github.com>
2020-01-22 23:13:45 -0500

    Commit Files
    first commit

---
+++ code.js
@@ -0,0 +1,11 @@
+var parentTask
+var childTask
+var clientDeadline
+var userAccountCreationDate
+
+//check if there is a parent task, and find completed dated of parent task
+if (parentTask == true) {
+
+} else {
+
+}
